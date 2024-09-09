583e45c1cbb06508a4eeca96be0225cf98349e86
Meyckooo <meyckocanoy@gmail.com>
2024-09-10 00:11:30 +0800

    Day 8 of Practicing JS
    Continuation of learning about Ternary Operator with different questions made by me

--- ternary-operator/ternary-operator.js
+++ ternary-operator/ternary-operator.js
@@ -7,3 +7,18 @@ const drink = age >= 18 ? 'I like to drink wine' : 'I like to drink water';
 console.log(drink);
 
 console.log(`I like to drink ${age >= 18 ? 'Wine' : 'Water'}`);
+
+
+// Exanple2
+
+const totalValue = 1300;
+
+const discount = totalValue >= 1250 && totalValue <= 1500 ? totalValue * 20/100 : totalValue * 50/100;
+
+console.log(`The total value of all items is ${totalValue} and the discount is ${discount}`)
+
+const perDay = 500 * 24;
+
+const salary = perDay >= 15000 && perDay <= 20000 ? `Your salary is ${perDay} and you belong to above salary wage` : `Your salary is ${perDay} and you belong to below or minimum salary wage`;
+
+console.log(salary);
