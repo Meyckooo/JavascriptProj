fb4fb5e0f96d509665de6fa288f6ca38089038ab
Meycko Jade Anthony Canoy <meyckocanoy@gmail.com>
2024-09-16 20:51:27 +0800

    Day 11 of Practicing JS
    - Reviewing Function

--- functions-calling-other-functions/functions-calling-other-functions.js
+++ functions-calling-other-functions/functions-calling-other-functions.js
@@ -1,3 +1,4 @@
+/*
 function cutFruitPieces(fruit){
     return fruit * 4;
 }
@@ -11,3 +12,25 @@ function fruitProcessor(apples, oranges){
 }
 
 console.log(fruitProcessor(2, 3));
+*/
+
+const calcAge = function (birthYear){
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+
+if (retirement > 0){
+    console.log(`${firstname} retire in ${retirement} years`);
+    return retirement;
+}else{
+    console.log(`${firstName} has already retired`);
+    return -1;
+}
+}
+
+console.log(yearsUntilRetirement(1991, 'Meycko'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
