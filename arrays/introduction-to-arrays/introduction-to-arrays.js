18d2ae68dee9140c8f4c250ea7bcaf6c40b858bc
Meycko Jade Anthony Canoy <meyckocanoy@gmail.com>
2024-09-17 20:36:08 +0800

    Day 12 of Practicing JS
    - Introduction to Array

---
+++ arrays/introduction-to-arrays/introduction-to-arrays.js
@@ -0,0 +1,24 @@
+const friend1 = 'Meycko';
+const friend2 = 'Mike';
+const friend3 = 'Xaviery';
+
+const friends = ['Meycko', 'Mike', 'Xaviery'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1])
+
+friends[2] = 'Jay';
+console.log(friends);
+// freinds = ['Bob', 'Alice']
+
+
+const firstName = 'Jonas';
+const jonas = [firstName, 'Schmedtmann' , 2037 - 1991, 'teacher', friends];
+console.log(jonas);
+console.log(jonas.length);
