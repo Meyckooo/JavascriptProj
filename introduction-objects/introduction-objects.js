2557848616c40b3c4955749ba44caae165fc766a
Meycko Jade Anthony Canoy <meyckocanoy@gmail.com>
2024-09-19 21:31:51 +0800

    Day 14 of Practicing JS
    - Introduction Objects

---
+++ introduction-objects/introduction-objects.js
@@ -0,0 +1,15 @@
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'Teacher',
+    ['Meycko', 'Mike', 'Xaviery'],
+];
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 1991,
+    job: 'Teacher',
+    friend: ['Meycko', 'Mike', 'Xaviery']
+};
