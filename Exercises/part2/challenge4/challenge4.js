b5bc53bc3095855bfeafef4118337539d83de13e
Meycko Canoy <meyckocanoy@gmail.com>
2025-01-23 18:24:47 +0800

    Part 2 Challenge 4
    - My Answer

--- Exercises/part2/challenge4/challenge4.js
+++ Exercises/part2/challenge4/challenge4.js
@@ -1,16 +1,17 @@
 const calcTip = function (bill) {
+    console.log(bill)
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
 
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52 ];
 
-let tips = [];
-let totals = [];
+let totals = 0;
 
 for(let i = 0; i < bills.length; i++){
-tips.push(calcTip[i]);
-totals.push(bills[i]);
+    let temp = bills[i]
+    totals += calcTip(temp)
 }
 
-console.log(tips);
-console.log(totals);
+
+// console.log(tips);
+console.log('total',totals / bills.length);
