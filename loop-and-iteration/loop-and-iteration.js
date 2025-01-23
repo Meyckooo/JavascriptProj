2f3a304e3206997b1f5ddd363e711fa31325a660
Meycko Canoy <meyckocanoy@gmail.com>
2025-01-23 17:32:29 +0800

    Done Lesson
    - For LOOP
    - Backwards Loop
    - Loops in a Loops
    - While LOOP

--- loop-and-iteration/loop-and-iteration.js
+++ loop-and-iteration/loop-and-iteration.js
@@ -48,3 +48,45 @@ console.log(meyckoArray[i], typeof meyckoArray[i]);
 }
 
 
+// LOOP BACKWARDS AND LOOPS IN LOOPS
+
+console.log('--- LOOP BACKWARDS ---');
+
+for (let i = meyckoArray.length -1; i >= 0; i--){
+    console.log(i, meyckoArray[i]);
+}
+
+console.log('--- AND LOOPS IN LOOPS ---');
+
+for(let exercise = 1; exercise < 6; exercise++){
+    console.log(`------ Starting Exercise ${exercise} -----`);
+
+    for(let rep = 1; rep < 6; rep++){
+        console.log(`Exercise ${exercise} Lifting Weight Repetition of ${rep}`);
+    }
+}
+
+console.log('--- WHILE LOOP ---');
+
+let piece = 1;
+const fruits = [
+    'Orange',
+    'Apple',
+    'Avocado',
+    'Mango'
+];
+
+while(piece < fruits.length){
+    console.log(`${piece} Piece of ${fruits[piece]}`);
+    piece++;
+}
+
+let dice = Math.trunc(Math.random() *6) + 1;
+
+while (dice !== 6){
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if(dice === 6){
+        console.log('Loop is about to end...');
+    }
+}
